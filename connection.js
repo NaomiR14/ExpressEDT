857cb0e8e76ca4dbeba27cb4f85d371465452725
NaomiR14 <adrianaroman433@gmail.com>
2025-07-28 13:33:59 -0600

    first commit

---
+++ connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const {mongodb} = require('./config')
+
+
+const connection = mongoose.connect(`mongodb+srv://${mongodb.user}:${mongodb.password}@${mongodb.host}/${mongodb.database}`)
+.then(()=>{
+    console.log('Conexion exitosa')
+}).catch((err)=>{
+    console.log('Ha ocurrido un error ' + err)
+})
+
+module.exports = connection
+
